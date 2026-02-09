c51ee6f3d81fab4666ae130bd966366643f4f0fc
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2026-02-09 08:51:42 +0100

    feat(bookService): service pour les livres

---
+++ ReadMe/Services/BookService.cs
@@ -0,0 +1,27 @@
+﻿using ReadMe.Models;
+using System.Collections.ObjectModel;
+
+namespace ReadMe.Services
+{
+    /// <summary>
+    /// Service centralisé responsable de la gestion des livres.
+    /// Il agit comme une source de vérité unique pour toute l'application.
+    /// </summary>
+    internal class BookService
+    {
+        /// <summary>
+        /// Liste observable contenant tous les livres de l'application.
+        /// ObservableCollection permet une mise à jour automatique de l'UI.
+        /// </summary>
+        public ObservableCollection<Book> Books { get; private set; } = [];
+
+        /// <summary>
+        /// Ajoute un nouveau livre à la collection globale.
+        /// </summary>
+        /// <param name="book">Livre à ajouter.</param>
+        public void AddDeck(Book book)
+        {
+            Books.Add(book);
+        }
+    }
+}
